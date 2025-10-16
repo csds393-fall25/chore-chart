a66e07662577e2a78381be9931739c53dbb74997
ShannonGriswold <shannon.v.griswold@gmail.com>
2025-10-15 20:05:14 -0400

    WIP some more ui done

--- client/src/pages/Chore.vue
+++ client/src/pages/Chore.vue
@@ -27,20 +27,75 @@
               variant="outlined"
               id="description"
               v-model="chore.description"
+              v-if="mode == 'create' || mode == 'edit'"
             ></v-textarea>
+            <p v-else class="text-subtitle-1">{{chore.description}}</p>
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="3" md="2">
-            <div class="text-subtitle-1 font-weight-bold">Description: </div>
+            <div class="text-subtitle-1 font-weight-bold"><span class="error" v-if="mode == 'create' || mode == 'edit'">*</span>Difficulty: </div>
           </v-col>
           <v-col cols="12" sm="9" md="6">
-            <v-textarea
-              placeholder="Description"
+            <v-combobox
+              placeholder="Difficulty"
+              required
               variant="outlined"
-              id="description"
-              v-model="chore.description"
-            ></v-textarea>
+              id="difficulty"
+              v-model="chore.difficulty"
+              :items="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+              v-if="mode == 'create' || mode == 'edit'"
+            ></v-combobox>
+            <p v-else class="text-subtitle-1">{{chore.difficulty}}</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="3" md="2">
+            <div class="text-subtitle-1 font-weight-bold"><span class="error" v-if="mode == 'create' || mode == 'edit'">*</span>Location: </div>
+          </v-col>
+          <v-col cols="12" sm="9" md="6">
+            <v-combobox
+              placeholder="Location"
+              required
+              variant="outlined"
+              id="location"
+              v-model="chore.location"
+              :items="['Kitchen', 'Dining Room', 'Living Room', 'Bedroom', 'Outside', 'Laundry Room', 'Bathroom', 'Office', 'Basement', 'Other']"
+              v-if="mode == 'create' || mode == 'edit'"
+            ></v-combobox>
+            <p v-else class="text-subtitle-1">{{chore.location}}</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="3" md="2">
+            <div class="text-subtitle-1 font-weight-bold"><span class="error" v-if="mode == 'create' || mode == 'edit'">*</span>Estimated Time To Complete: </div>
+          </v-col>
+          <v-col cols="12" sm="9" md="6">
+            <v-text-field
+              placeholder="hours"
+              required
+              variant="outlined"
+              id="timeToComplete"
+              v-model="chore.timeToComplete"
+              v-if="mode == 'create' || mode == 'edit'"
+              ></v-text-field>
+              <div v-else class="text-subtitle-1">{{ chore.timeToComplete }}</div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="3" md="2">
+            <div class="text-subtitle-1 font-weight-bold">Due Date: </div>
+          </v-col>
+          <v-col cols="12" sm="9" md="6">
+            <v-text-field
+              type="date"
+              required
+              variant="outlined"
+              id="timeToComplete"
+              v-model="chore.timeToComplete"
+              v-if="mode == 'create' || mode == 'edit'"
+              ></v-text-field>
+              <div v-else class="text-subtitle-1">{{ chore.timeToComplete }}</div>
           </v-col>
         </v-row>
       </v-form>
