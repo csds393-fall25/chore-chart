272725009183a5f4e4b95930d831dc579a9df93b
Mollie Ackerman <molliesa2@gmail.com>
2025-11-05 21:17:32 -0500

    error messages show validation on screen and disappear when information is correct

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -13,18 +13,18 @@
           {{isCreate ?  'Create Profile' : 'Login'}}
         </h2>
         <v-form>
-          <v-text-field  v-if="isCreate"  v-model = "displayedName"  label = "Name"></v-text-field>
-          <v-text-field   v-model = "username"  label = "Email"></v-text-field>
-          <v-text-field  v-model = "password"  label = "Password" type = "password" :persistent-hint='isIncorrect' :hint = " isIncorrect ? 'Incorrect username or password' : ''" ></v-text-field>
-          <v-text-field v-if="isCreate" v-model = "repeatedPassword"  label = "Password Again" type = "password" :persistent-hint='isNotSame' :hint = " isNotSame ? 'Passwords do not match' : ''" ></v-text-field>   
+          <v-text-field   v-if="isCreate" :error-messages="errorMessages.name" v-model = "displayedName"  label = "Name"></v-text-field>
+          <v-text-field  :error-messages="errorMessages.email" v-model = "username"  label = "Email"></v-text-field>
+          <v-text-field  :error-messages="errorMessages.password" v-model = "password"  label = "Password" type = "password"  ></v-text-field>
+          <v-text-field v-if="isCreate" :error-messages="errorMessages.repeatedPassword" v-model = "repeatedPassword"  label = "Password Again" type = "password" :persistent-hint='isNotSame'  ></v-text-field>   
           <v-row v-if="isCreate" class="mx-auto my-auto">
             <v-col  v-if="isCreate" style = "font-size: x-small;" cols = "6">
               Estimated Time To Complete Chores (minutes)
-              <v-number-input v-if="isCreate" v-model = "estimatedTime" :min = '0' control-variant="split" ></v-number-input>
+              <v-number-input v-if="isCreate" :error-messages="errorMessages.estTime" v-model = "estimatedTime" :min = '0' control-variant="split" ></v-number-input>
             </v-col>
             <v-col v-if="isCreate"  style = "font-size: x-small;" cols = "6">
               Maximum Difficulty
-              <v-number-input v-if="isCreate" v-model = "maxDifficulty" :min = '1' :max = '10' control-variant="split"  ></v-number-input>
+              <v-number-input v-if="isCreate" :error-messages="errorMessages.maxDiff" v-model = "maxDifficulty" :min = '1' :max = '10' control-variant="split"  ></v-number-input>
             </v-col>
 
             <v-dialog v-model="showDialog" width="auto">
@@ -67,12 +67,19 @@
   import { useAppStore } from '@/stores/app.js';
 
   const username = ref();
-  const password = ref();
+  const usernameError = ref("Email must follow format xxx@xxx.xxx")
+  const password = ref("");
+  const passwordError = ref("Password must be 8-25 characters and include at least one capital letter and one number")
   const isIncorrect = ref(false);
   const displayedName = ref()
+  const nameError = ref("Name must exist")
   const maxDifficulty = ref()
+  const maxDiffError = ref("Maximum difficulty must be between 1 and 10")
   const estimatedTime = ref()
-  const repeatedPassword = ref()
+  const estTimeError = ref("Estimated time must be greater than 0")
+  const repeatedPassword = ref("")
+  const repeatedPasswordError = ref("Passwords much match")
+  const errorMessages = ref({name: "", email: "", password: "", repeatedPassword: "", estTime: "", maxDiff: ""})
   const isNotSame = ref(false)
   const isCreate = ref(false)
   const store = useAppStore()
@@ -131,7 +138,11 @@
     let flag = true;
   if(!displayedName.value){
     console.log("Name is wrong")
+    errorMessages.value.name = "name must exist"
     flag = false;
+  }
+  else{
+    errorMessages.value.name = ""
   }
 
   let regex = username.value.match(/\w+@\w+\.\w+/)
@@ -139,23 +150,48 @@
 
   if(!username.value || !regex  ){
     console.log("email is wrong")
+    errorMessages.value.email = "Email must follow format xxx@xxx.xxx"
     flag = false
   }
+  else{
+    errorMessages.value.email = ""
+  }
   
-  if (typeof(password.value) != null && typeof(repeatedPassword.value) != null && password.value != repeatedPassword.value){
+  if (!password.value || password.value.length < 8 || password.value.length > 25 || !(password.value.match(/\.*\d\.*/) && password.value.match(/\.*[A-Z]\.*/) ) ){
     console.log("password is wrong")
-      isNotSame.value = true;
+    errorMessages.value.password = "Password must be 8-25 characters and include at least one capital letter and one number"
       flag = false
     }
+    else{
+      errorMessages.value.password = ""
+    }
+
+  if(!(repeatedPassword.value == password.value)){
+    flag = false
+    errorMessages.value.repeatedPassword = "Passwords do not match"
+
+  }
+  else{
+    errorMessages.value.repeatedPassword = ""
+  }
 
   if(!estimatedTime.value){
     console.log("estimated time is wrong")
+    errorMessages.value.estTime = "Estimated time must be greater than 0"
     flag = false
+  }
+  else{
+    errorMessages.value.estTime = ""
+
   }
 
   if(!maxDifficulty.value){
     console.log("max difficulty is wrong")
+    errorMessages.value.maxDiff = "Maximum difficulty must be between 1 and 10"
     flag =  false
+  }
+  else{
+    errorMessages.value.maxDiff = ""
   }
   if (flag){
 
