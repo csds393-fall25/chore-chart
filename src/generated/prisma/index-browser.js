0ceb19a845c3bac27244e7410bd3dda6ccf5063b
Thao Nguyen <thao031103@gmail.com>
2025-11-10 17:03:22 -0500

    Add skintone to list of props

--- src/generated/prisma/index-browser.js
+++ src/generated/prisma/index-browser.js
@@ -155,6 +155,7 @@ exports.Prisma.ChoreScalarFieldEnum = {
 
 exports.Prisma.AvatarScalarFieldEnum = {
   ownerId: 'ownerId',
+  skinToneId: 'skinToneId',
   hatId: 'hatId',
   hairId: 'hairId',
   shirtId: 'shirtId',
@@ -198,7 +199,8 @@ exports.PropType = exports.$Enums.PropType = {
   hair: 'hair',
   shirt: 'shirt',
   background: 'background',
-  handProp: 'handProp'
+  handProp: 'handProp',
+  skinTone: 'skinTone'
 };
 
 exports.Prisma.ModelName = {
