316e72cb88d95b87bb9317b9fd5f32116d53ba0b
Mollie Ackerman <molliesa2@gmail.com>
2025-11-11 19:53:56 -0500

    validation is added to profile and profile is no longer automatically on edit mode

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -2,15 +2,21 @@
   <v-sheet class="fill-height w-100 " color="primary-darken-1">
     <v-avatar class="ma-4" size="large" color="primary"></v-avatar>
     <v-form class="ma-4">
-      <v-text-field style="width: 75%; " label="Name" v-model="name"></v-text-field>
-      <v-text-field :disabled="true" style="width: 75%" v-model="username" label="Email"></v-text-field>
-      Estimated Time To Complete Chores (minutes)
-      <v-number-input style="width: 75%" v-model="estimatedTime" :min='0' control-variant="split"></v-number-input>
-      Maximum Difficulty
-      <v-number-input style="width: 75%" v-model="maxDifficulty" :min='1' :max='10'
+      <v-text-field v-if="isUpdate" :error-messages="errorMessages.name" style="width: 75%; " label="Name" v-model="name"></v-text-field>
+      <p v-if="!isUpdate">Name: {{name}}</p>
+      <v-text-field v-if="isUpdate" :error-messages="errorMessages.email" :disabled="true" style="width: 75%" v-model="username" label="Email"></v-text-field>
+      <!-- <v-text-field :error-messages="errorMessages.password" style="width: 75%; " label="password" v-model="password"></v-text-field>
+      <v-text-field :error-messages="errorMessages.repeatedPassword" style="width: 75%" v-model="repeatedPassword" label="verify password"></v-text-field> -->
+      <p v-if="!isUpdate">Email: {{username}}</p>
+      <p v-if="!isUpdate"> Estimated Time To Complete Chores (minutes): {{estimatedTime}}</p>
+      <p v-if="isUpdate">Estimated Time To Complete Chores (minutes)</p>
+      <v-number-input v-if="isUpdate" :disabled="!isUpdate" :error-messages="errorMessages.estTime" style="width: 75%" v-model="estimatedTime" :min='0' control-variant="split"></v-number-input>
+        <p v-if="!isUpdate">Maximum Difficulty: {{maxDifficulty}}</p>
+      <p v-if="isUpdate">Maximum Difficulty</p>
+      <v-number-input v-if="isUpdate" :disabled="!isUpdate" :error-messages="errorMessages.maxDiff" style="width: 75%" v-model="maxDifficulty" :min='1' :max='10'
         control-variant="split"></v-number-input>
       <v-btn class="elevation-0" style="background-color: #51d299 ; font-size: small; "
-        @click=" updateProfile()">Update</v-btn>
+        @click=" !isUpdate ? updateButton() : updateProfile()">Update</v-btn>
       <v-btn class="mx-auto my-auto elevation-0 ml-4" color="error" @click="showDialog = true"
         style=" font-size: small; "> Delete</v-btn>
       <v-dialog v-model="showDialog" width="auto">
@@ -31,12 +37,25 @@ import { useAppStore } from "../stores/app.js";
 import FetchService from '@/FetchService';
 const store = useAppStore();
 const username = ref(store.user.email);
+const isUpdate = ref(false)
+const password = ref();
+const repeatedPassword = ref();
 const name = ref(store.user.name)
 const maxDifficulty = ref(store.user.difficulty)
 const estimatedTime = ref(store.user.maxChoreTime)
 const showDialog = ref(false)
+const errorMessages = ref({name: "", email: "", password: "", repeatedPassword: "", estTime: "", maxDiff: "", houseName: "", jc: ""})
+
+function updateButton(){
+  isUpdate.value = true
+}
 
 async function updateProfile() {
+
+  if(!validateProfile()){
+    return
+
+  }
  
   const result = await FetchService.updateUser(store.user.id, {
     name: name.value,
@@ -59,4 +78,67 @@ function deleteProfile(id) {
   store.loggedIn = false
   return result;
 }
+
+function validateProfile(){
+    let flag = true;
+  if(!name.value){
+    console.log("Name is wrong")
+    errorMessages.value.name = "name must exist"
+    flag = false;
+  }
+  else{
+    errorMessages.value.name = ""
+  }
+
+
+
+
+  // if (!password.value || password.value.length < 8 || password.value.length > 25 || !(password.value.match(/\.*\d\.*/) && password.value.match(/\.*[A-Z]\.*/) ) ){
+  //   console.log("password is wrong")
+  //   errorMessages.value.password = "Password must be 8-25 characters and include at least one capital letter and one number"
+  //     flag = false
+  //   }
+  //   else{
+  //     errorMessages.value.password = ""
+  //   }
+
+  // if(!(repeatedPassword.value == password.value)){
+  //   flag = false
+  //   errorMessages.value.repeatedPassword = "Passwords do not match"
+
+  // }
+  // else{
+  //   errorMessages.value.repeatedPassword = ""
+  // }
+
+  if(!estimatedTime.value || estimatedTime.value <= 0){
+    console.log("estimated time is wrong")
+    errorMessages.value.estTime = "Estimated time must be greater than 0"
+    flag = false
+  }
+  else{
+    errorMessages.value.estTime = ""
+
+  }
+
+  if(!maxDifficulty.value || maxDifficulty.value < 1 || maxDifficulty.value > 10){
+    console.log("max difficulty is wrong")
+    errorMessages.value.maxDiff = "Maximum difficulty must be between 1 and 10"
+    flag =  false
+  }
+  else{
+    errorMessages.value.maxDiff = ""
+  }
+  if (flag){
+
+
+  errorMessages.value.houseName = ""
+  errorMessages.value.jc = ""
+  }
+
+  return flag
+
+}
+
+
 </script>
