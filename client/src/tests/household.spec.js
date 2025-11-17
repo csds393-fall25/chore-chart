11189045cd412521a75cbdf9f1d8a8b90bfe3148
Mollie Ackerman <molliesa2@gmail.com>
2025-11-17 08:50:13 -0500

    tests written for leaving household

--- client/src/tests/household.spec.js
+++ client/src/tests/household.spec.js
@@ -69,7 +69,7 @@ expect(wrapper.text()).toContain(wrapper.vm.members[1].name)
 
 })
 
-test("LH-1 Leave as not last member and provide valid input", async () => {
+test("LH-1 Leave as not last member and provide valid input (create new household)", async () => {
     const wrapper = mount(Household, {
         global: {
             plugins: [
@@ -118,33 +118,300 @@ test("LH-1 Leave as not last member and provide valid input", async () => {
     })
 
     const store = useAppStore()
-
-console.log(store.user)
-wrapper.vm.showDialog = true
-wrapper.vm.householdName = "THouse"
-
-//  const spy = vi.spyOn(wrapper.vm, "createNewHousehold");
-//     await wrapper.find("#newHouse").trigger("click")
-//     await nextTick()
-//     expect(spy).toHaveBeenCalled();
-    wrapper.vm.createNewHousehold()
+    wrapper.vm.showDialog = true
+    wrapper.vm.householdName = "THouse"
+    await wrapper.vm.createNewHousehold()
     await nextTick()
-    console.log("join Code")
-    console.log(store.household.name)
-    console.log(store.household.joinCode)
-    console.log(wrapper.vm.householdName)
-    console.log(wrapper.vm.joinCode)
     const houseResult = await FetchService.fetchHouseholdByJoin(store.household.joinCode)
-    console.log("houseResult")
-    console.log(houseResult)
-        console.log(store.household.joinCode)
-
-    //expect(store.household.).toBe(houseResult)
-
-
-
-
-
-
+    expect(store.household.id).toBe(houseResult.id)
 
 })
+
+test("LH-1 Leave as not last member and provide valid input (join an existing household)", async () => {
+    const wrapper = mount(Household, {
+        global: {
+            plugins: [
+                createTestingPinia({
+                    createSpy: vi.fn,
+                    initialState: {
+                        app: {
+                            user: {
+                                id: 0,
+                                householdId: 1,
+                                role: "leader",
+                            },
+                            household: {
+
+                                users: [
+                                    {
+                                        id: 0,
+                                        name: "test",
+                                        role: "member",
+                                    },
+                                 {
+                                id: 482,
+                                name: "test2",
+                                householdId: 1,
+                                role: "leader",
+                            },
+                                ],
+                            },
+                        },
+                    },
+                }),
+                [vuetify],
+            
+            ],
+
+             stubs: {
+    VDialog: {
+      name: "VDialog",
+      template: '<div class="v-dialog-stub"><slot /></div>',
+      props: ['modelValue',
+      ]
+    }
+  }
+        },
+       
+    })
+
+    const store = useAppStore()
+    wrapper.vm.showDialog = true
+    wrapper.vm.householdName = "Codorado1234"
+    await wrapper.vm.joinNewHousehold()
+    await nextTick()
+    const houseResult = await FetchService.fetchHouseholdByJoin(store.household.joinCode)
+    expect(store.household.id).toBe(houseResult.id)
+
+})
+
+test("createNewHousehold is run when button is pressed ", async () => {
+    const wrapper = mount(Household, {
+        global: {
+            plugins: [
+                createTestingPinia({
+                    createSpy: vi.fn,
+                    initialState: {
+                        app: {
+                            user: {
+                                id: 0,
+                                householdId: 1,
+                                role: "leader",
+                            },
+                            household: {
+
+                                users: [
+                                    {
+                                        id: 0,
+                                        name: "test",
+                                        role: "member",
+                                    },
+                                 {
+                                id: 482,
+                                name: "test2",
+                                householdId: 1,
+                                role: "leader",
+                            },
+                                ],
+                            },
+                        },
+                    },
+                }),
+                [vuetify],
+            
+            ],
+
+             stubs: {
+    VDialog: {
+      name: "VDialog",
+      template: '<div class="v-dialog-stub"><slot /></div>',
+      props: ['modelValue',
+      ]
+    }
+  }
+        },
+       
+    })
+
+    const store = useAppStore()
+    wrapper.vm.showDialog = true
+    wrapper.vm.householdName = "THouse"
+ const spy = vi.spyOn(wrapper.vm, "createNewHousehold");
+    await wrapper.find("#newHouse").trigger("click")
+    await nextTick()
+    expect(spy).toHaveBeenCalled();
+})
+
+test("joinNewHousehold is run when button is pressed ", async () => {
+    const wrapper = mount(Household, {
+        global: {
+            plugins: [
+                createTestingPinia({
+                    createSpy: vi.fn,
+                    initialState: {
+                        app: {
+                            user: {
+                                id: 0,
+                                householdId: 1,
+                                role: "leader",
+                            },
+                            household: {
+
+                                users: [
+                                    {
+                                        id: 0,
+                                        name: "test",
+                                        role: "member",
+                                    },
+                                 {
+                                id: 482,
+                                name: "test2",
+                                householdId: 1,
+                                role: "leader",
+                            },
+                                ],
+                            },
+                        },
+                    },
+                }),
+                [vuetify],
+            
+            ],
+
+             stubs: {
+    VDialog: {
+      name: "VDialog",
+      template: '<div class="v-dialog-stub"><slot /></div>',
+      props: ['modelValue',
+      ]
+    }
+  }
+        },
+       
+    })
+
+    const store = useAppStore()
+    wrapper.vm.showDialog = true
+ const spy = vi.spyOn(wrapper.vm, "joinNewHousehold");
+    await wrapper.find("#existingHouse").trigger("click")
+    await nextTick()
+    expect(spy).toHaveBeenCalled();
+})
+
+
+test("LH-3 Invalid input (create new household)", async () => {
+    const wrapper = mount(Household, {
+        global: {
+            plugins: [
+                createTestingPinia({
+                    createSpy: vi.fn,
+                    initialState: {
+                        app: {
+                            user: {
+                                id: 0,
+                                householdId: 1,
+                                role: "leader",
+                            },
+                            household: {
+
+                                users: [
+                                    {
+                                        id: 0,
+                                        name: "test",
+                                        role: "member",
+                                    },
+                                 {
+                                id: 482,
+                                name: "test2",
+                                householdId: 1,
+                                role: "leader",
+                            },
+                                ],
+                            },
+                        },
+                    },
+                }),
+                [vuetify],
+            
+            ],
+
+             stubs: {
+    VDialog: {
+      name: "VDialog",
+      template: '<div class="v-dialog-stub"><slot /></div>',
+      props: ['modelValue',
+      ]
+    }
+  }
+        },
+       
+    })
+
+    const store = useAppStore()
+    wrapper.vm.showDialog = true
+    wrapper.vm.householdName = ""
+    await wrapper.vm.createNewHousehold()
+    await nextTick()
+    expect(wrapper.vm.errorMessages.household).toBe("Household name must be below 50 characters and have at least 1 letter")
+
+})
+
+test("LH-3 Invalid input (join an existing household)", async () => {
+    const wrapper = mount(Household, {
+        global: {
+            plugins: [
+                createTestingPinia({
+                    createSpy: vi.fn,
+                    initialState: {
+                        app: {
+                            user: {
+                                id: 0,
+                                householdId: 1,
+                                role: "leader",
+                            },
+                            household: {
+
+                                users: [
+                                    {
+                                        id: 0,
+                                        name: "test",
+                                        role: "member",
+                                    },
+                                 {
+                                id: 482,
+                                name: "test2",
+                                householdId: 1,
+                                role: "leader",
+                            },
+                                ],
+                            },
+                        },
+                    },
+                }),
+                [vuetify],
+            
+            ],
+
+             stubs: {
+    VDialog: {
+      name: "VDialog",
+      template: '<div class="v-dialog-stub"><slot /></div>',
+      props: ['modelValue',
+      ]
+    }
+  }
+        },
+       
+    })
+
+    const store = useAppStore()
+    wrapper.vm.showDialog = true
+    wrapper.vm.householdName = ""
+    await wrapper.vm.joinNewHousehold()
+    await nextTick()
+    expect(wrapper.vm.errorMessages.household).toBe("Join code does not exist" )
+})
+
+
+
+
