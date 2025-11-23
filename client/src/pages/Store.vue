63041e9fd3c0d2b2ae41e2eed129ca42d59e2622
ShannonGriswold <shannon.v.griswold@gmail.com>
2025-11-23 16:40:35 -0500

    Updated the store ui

--- client/src/pages/Store.vue
+++ client/src/pages/Store.vue
@@ -1,34 +1,43 @@
 <template>
   <v-sheet color="navy" class="fill-height pr-0">
     <v-container>
-      <v-row class="mb-3 text-h3 justify-center">
+      <v-row class="mb-3 text-h2 pl-3">
         <div>Store</div>
       </v-row>
 
       <v-row class="mb-3">
-        <v-col cols="12" sm="6" md="4"
+        <v-col cols="12" sm="4" md="3"
           v-for="(prop) in propsList"
           :key="prop.id"
         >
-          <v-card class="pr-2">
-            <v-card-item class="pr-0">
-              <template v-slot:prepend>
-                <img height="60" width="60" :src="prop.url" alt="prop image">
-              </template>
+          <v-card>
+            <v-card-item class="pr-0 pl-0 w-100 text-center">
+                <img class="pl-0" :src="prop.url" alt="prop image">
             </v-card-item>
             <v-card-text class="text-center text-body-1">
               {{ prop.name }}
             </v-card-text>
             <v-card-actions>
               <v-btn
-                class="w-50"
+                v-if="!isOwned(prop)"
+                class="w-100"
                 color="secondary"
                 variant="elevated"
                 density="compact"
                 @click="buyProp(prop)"
-                :disabled="isOwned(prop)"
               >
-                {{ isOwned(prop) ? 'Owned' : 'Buy' }}
+                Buy
+              </v-btn>
+              <v-btn
+                v-else
+                class="w-100"
+                color="secondary"
+                variant="elevated"
+                density="compact"
+                @click="equipProp(prop)"
+                :disabled="isEquipped(prop)"
+              >
+                {{ isEquipped(prop) ? 'Equipped' : 'Equip' }}
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -39,35 +48,95 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-import { useAppStore } from '@/stores/app'
-import FetchService from '../FetchService.js'
+  import { onMounted, ref } from 'vue'
+  import { useAppStore } from '@/stores/app'
+  import FetchService from '../FetchService.js'
 
-const store = useAppStore()
+  const store = useAppStore()
 
-const propsList = ref([])
+  const ownedProps = ref([])
+  const propsList = ref([])
 
-onMounted(async () => {
-  if (store.allProps.length == 0) {
-    const result = await FetchService.getAvatarProps()
-    store.allProps = result
-    propsList.value = result
-  } else {
-    propsList.value = store.allProps
+  const usersAvatar = ref(store.avatars.find((avatar) => avatar.userId == store.user.id))
+
+  onMounted(async () => {
+    if (store.allProps.length == 0) {
+      const result = await FetchService.getAvatarProps()
+      store.allProps = result
+      propsList.value = result
+    } else {
+      propsList.value = store.allProps
+    }
+
+    //TODO: retrieve the props owned by the user and store them in ownedProps
+
+    //If the user's avatar couldn't be found, fetch it
+    if(!usersAvatar.value) {
+      let avatar = ref({
+        userId: store.user.id,
+        skinTone: "",
+        hat: "",
+        hair: "",
+        shirt: "",
+        background: "",
+        handProp: ""
+      });
+
+      const avatarFetch = await FetchService.getAvatar(store.user.id);
+      avatarFetch.forEach((prop) => {
+        switch(prop.type) {
+          case "skinTone":
+            avatar.value.skinTone = prop.url;
+            break;
+          case "hat":
+            avatar.value.hat = prop.url;
+            break;
+          case "hair":
+            avatar.value.hair = prop.url;
+            break;
+          case "shirt":
+            avatar.value.shirt = prop.url;
+            break;
+          case "background":
+            avatar.value.background = prop.url;
+            break;
+          case "handProp":
+            avatar.value.handProp = prop.url;
+            break;
+          default:
+            console.log(`Unexpected prop type ${prop.type}`);
+        }
+      });
+      store.avatars.push = avatar.value;
+      usersAvatar.value = avatar.value
+    }
+  })
+
+  function isOwned(prop) {
+    return ownedProps.value.some((ownedProp) => ownedProp.id == prop.id)
   }
-})
 
-function isOwned(prop) {
-  // TODO: implement fetching list of props the user owns
-}
+  function buyProp(prop) {
+    ownedProps.value.push(prop)
 
-function buyProp(prop) {
-  // TODO: implement buy prop function
-}
+    // TODO: implement buy prop function from database
+  }
+
+  function equipProp(prop) {
+    usersAvatar.value[prop.type] = prop.url;
+    store.avatars.find((avatar) => avatar.userId == store.user.id)[prop.type] = prop.url
+
+    // TODO: implement updating the users avatar in the database
+  }
+
+  function isEquipped(prop) {
+    return Object.entries(usersAvatar.value).some((entry) => entry[1] == prop.url)
+  }
 </script>
 
 <style scoped>
-.list-append {
-  width: 100px;
+img {
+  width: 90%;
+  margin: auto;
 }
 </style>
