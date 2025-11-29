901ccb7372d8a8c44544bde8aaf88b910f158c8c
Mollie Ackerman <molliesa2@gmail.com>
2025-11-29 16:58:58 -0500

    fixed testHousehold not deleting

--- client/src/tests/profile.spec.js
+++ client/src/tests/profile.spec.js
@@ -243,7 +243,10 @@ test("PDT-1 delete user information", async () => {
 
     //TODO fails here sometimes
     const method = await wrapperProfile.vm.deleteProfile(result.id)
+    let house = await FetchService.fetchHouseholdByJoin(store.household.joinCode)
+    await FetchService.deleteHousehold(house.id)
     expect (method.deleted).toBe(true)
+    
 
 })
 
