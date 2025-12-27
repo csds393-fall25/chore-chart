3839d22b5f57b3c58e467db5101a9bad0f195ab0
Thao Nguyen <thao031103@gmail.com>
2025-12-26 19:32:19 -0500

    Migrate to supabase

--- server/app.js
+++ server/app.js
@@ -7,22 +7,9 @@ import cors from 'cors';
 import prisma from './prisma.js';
 import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt';
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import supabase from './supabase.js';
 
-const databaseUrl = process.env.DATABASE_URL
 const bucketName = process.env.BUCKET_NAME
-const bucketRegion = process.env.BUCKET_REGION
-const accessKey = process.env.ACCESS_KEY
-const secretAccessKey = process.env.SECRET_ACCESS_KEY
-
-const s3 = new S3Client({
-  credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey,
-  },
-  region: bucketRegion
-})
 
 const saltRounds = 10;
 
@@ -416,17 +403,11 @@ app.get('/api/avatar/:userId', async (req, res) => {
     for (const part of avatarParts) {
       // check that part has a name before trying to generate a signed URL
       if (!part || !part.name) continue;
-
-      const getPartParams = {
-        Bucket: bucketName,
-        Key: `avatar-props/${part.name}.png`,
-      };
-      const command = new GetObjectCommand(getPartParams);
       try {
-        const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-        part.url = url;
+        const { data } = supabase.storage.from(bucketName).getPublicUrl(`avatar-props/${part.name}.png`)
+        part.url = data.publicUrl;
       } catch (err) {
-        console.error('Failed to generate signed URL for', part.name, err?.message || err);
+        console.error('Failed to generate URL for', part.name, err?.message || err);
       }
     }
 
@@ -441,16 +422,11 @@ app.get('/api/avatar-props', async (req, res) => {
   try {
     const props = await prisma.avatarProp.findMany();
     for (const prop of props) {
-      const getPropsParams = {
-        Bucket: bucketName,
-        Key: `avatar-props/${prop.name}.png`
-      }
-      const command = new GetObjectCommand(getPropsParams);
       try {
-          const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-          prop.url = url;
+        const { data } = supabase.storage.from(bucketName).getPublicUrl(`avatar-props/${prop.name}.png`)
+        prop.url = data.publicUrl;
       } catch (err) {
-        console.error('Failed to generate signed URL for', part.name, err?.message || err);
+        console.error('Failed to generate URL for', prop.name, err?.message || err);
       }
     };
     res.json(props);
